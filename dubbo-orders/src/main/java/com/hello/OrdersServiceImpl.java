6128d5541d20a6c4fcf9166c32fca8a32df63fde
liuxiaoxiao <17521752353@163.com>
2018-10-26 22:06:34 +0800

    file

--- dubbo-orders/src/main/java/com/hello/OrdersServiceImpl.java
+++ dubbo-orders/src/main/java/com/hello/OrdersServiceImpl.java
@@ -2,6 +2,12 @@ package com.hello;
 
 public class OrdersServiceImpl implements IOrdersService {
     public Orders query(Long orderId) {
-        return null;
+        Orders orders=new Orders();
+        orders.setPrice(1000);
+        orders.setBuyName("悟空");
+        orders.setAddr("上海");
+        orders.setOrderId(1001);
+        orders.setProductName("三星s10");
+        return orders;
     }
 }
